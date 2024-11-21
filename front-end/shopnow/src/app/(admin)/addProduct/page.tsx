99549bc03156943330d4189a0abaec8eb894172c
sabaattaa <sabatta997@gmail.com>
2024-11-21 23:27:14 +0500

    updated

--- front-end/shopnow/src/app/(admin)/addProduct/page.tsx
+++ front-end/shopnow/src/app/(admin)/addProduct/page.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      add product
+    <div className="red w-100 d-flex p-2">
+      <div className="blue">img</div>
+      <div className="red w-100">remaning content</div>
+     
+     
     </div>
-  )
+  );
 }
 
 export default Page
